eb5ab05c71ba062cd579303a06ab308cbc7f48a9
Vladimir Sibirov <trustmaster@kodigy.com>
2019-12-25 13:01:31 +0100

    Add date picker

--- src/components/Content/Post/Date/Date.js
+++ src/components/Content/Post/Date/Date.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-function Date({ date }) {
-    return <span>{date.toLocaleDateString()}</span>;
+function DateEditable({ value, onClick }) {
+    return <span className="Date" onClick={onClick}>{value}</span>;
+}
+
+class Date extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: props.date,
+        };
+    }
+    handleChange = (date) => {
+        this.setState({ date });
+    }
+    render() {
+        return (<ReactDatePicker
+            selected={this.state.date}
+            onChange={this.handleChange}
+            customInput={<DateEditable />}
+        />);
+    }
 }
 
 export default Date;
