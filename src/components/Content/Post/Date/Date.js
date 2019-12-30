4b66197c1c8a093100155f0e1fd1dfddae6e09f6
Vladimir Sibirov <trustmaster@kodigy.com>
2019-12-30 22:21:01 +0100

    Forward ref for date picker

--- src/components/Content/Post/Date/Date.js
+++ src/components/Content/Post/Date/Date.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const DateEditable = ({ value, onClick }) => {
-    return <span className="Date" onClick={onClick}>{value}</span>;
-}
+const DateEditable = React.forwardRef(({ value, onClick }, ref) => {
+    return <span ref={ref} className="Date" onClick={onClick}>{value}</span>;
+})
 
 const Date = ({ date, onChange }) => {
     return (<ReactDatePicker
