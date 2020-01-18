8e91c4877d2fd8b3d894a4b3fc8b4fed5bffab55
Vladimir Sibirov <trustmaster@kodigy.com>
2020-01-18 18:28:28 +0100

    Use MediumEditor directly

--- src/components/Content/Editable/Editor/Editor.jsx
+++ src/components/Content/Editable/Editor/Editor.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import MediumEditor from 'react-medium-editor';
+import MediumEditor from 'medium-editor';
 import 'medium-editor/dist/css/medium-editor.css';
 import 'medium-editor/dist/css/themes/default.css';
 
-const Editor = ({ rich, text, onChange }) => {
-    const opts = {
-        toolbar: { buttons: [] },
-        forcePlainText: true,
-        cleanPastedHTML: true,
-        disableReturn: true,
-    };
-    if (rich !== undefined) {
-        opts.forcePlainText = false;
-        opts.toolbar = { buttons: ['bold', 'italic', 'underline', 'quote', 'orderedlist', 'unorderedlist', 'removeFormat'] };
-        opts.disableReturn = false;
-    }
-    return (
-        <div className="Editor">
-            <MediumEditor text={text} onChange={onChange} options={opts} />
-        </div>
-    );
-};
+export default class Editor extends React.Component {
+    componentDidMount() {
+        const dom = this.node;
+        const { rich, onChange } = this.props;
 
-export default Editor;
+        const opts = {
+            toolbar: { buttons: [] },
+            forcePlainText: true,
+            cleanPastedHTML: true,
+            disableReturn: true,
+            disableExtraSpaces: true,
+        };
+        if (rich !== undefined) {
+            opts.forcePlainText = false;
+            opts.toolbar = { buttons: ['bold', 'italic', 'underline', 'quote', 'orderedlist', 'unorderedlist', 'removeFormat'] };
+            opts.disableReturn = false;
+        }
+
+        this.medium = new MediumEditor(dom, opts);
+        this.medium.subscribe('editableInput', () => {
+            onChange(dom.innerHTML, this.medium);
+        });
+    }
+
+    componentDidUpdate() {
+        this.medium.restoreSelection();
+    }
+
+    componentWillUnmount() {
+        this.medium.destroy();
+    }
+
+    render() {
+        const { text, onChange } = this.props;
+
+        const html = { __html: text };
+
+        if (this.medium) {
+            this.medium.saveSelection();
+        }
+
+        return (<div ref={(node) => { this.node = node; return node; }} className="Editor" dangerouslySetInnerHTML={html} onChange={onChange} />);
+    }
+}
